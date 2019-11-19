8720cc7bb4eba4cb6de5f7c994fb33bfe05c147b
Serhiy Bednarskyi <sbedna@softserveinc.com>
2019-11-19 18:04:55 +0200

    fixed files issue

---
+++ RestaurantApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using RestaurantApp.Models;
+using Xamarin.Forms;
+
+namespace RestaurantApp.Pages
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterPage.listView.ItemSelected += OnItemSelected;
+
+            //иварва
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem is MasterPageItem item)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                MasterPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
